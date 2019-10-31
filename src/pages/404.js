b0b75048d626afc0e61eba67e898293e725a4e3a
stefantrinh1 <stefantrinh1.work@gmail.com>
2019-10-31 00:03:09 -0600

    adding 404 page text change

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,8 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a Page that doesn&#39;t exist... </p>
+    <h1>PAGE NOT FOUND</h1>
+    <p>The current page does not exist or has been deleted.</p>
 
     <Link to="/">Return To Homepage</Link>
     
